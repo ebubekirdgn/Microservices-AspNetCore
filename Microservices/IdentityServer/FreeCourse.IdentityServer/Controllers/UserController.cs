34f88cf75d41a3b13c8967c1eef5776e1a488a07
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-02-16 20:14:44 +0300

    updated

--- Microservices/IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
+++ Microservices/IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 using static IdentityServer4.IdentityServerConstants;
@@ -30,14 +31,30 @@ namespace FreeCourse.IdentityServer.Controllers
             {
                 UserName = signupDto.Username,
                 Email = signupDto.Email,
-                City= signupDto.City,
+                City = signupDto.City
             };
-            var result = await _userManager.CreateAsync(user,signupDto.Password);
+
+            var result = await _userManager.CreateAsync(user, signupDto.Password);
+
             if (!result.Succeeded)
             {
-                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(),400));
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 400));
             }
+
             return NoContent();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            var userIdClaim = User.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Sub);
+
+            if (userIdClaim == null) return BadRequest();
+
+            var user = await _userManager.FindByIdAsync(userIdClaim.Value);
+
+            if (user == null) return BadRequest();
+
+            return Ok(new { Id = user.Id, UserName = user.UserName, Email = user.Email, City = user.City });
         }
     }
 }
