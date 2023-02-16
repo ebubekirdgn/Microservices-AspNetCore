3736592836b911548eeb0df7d50e1d2237a83afb
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-02-16 20:30:40 +0300

    update Config

--- Microservices/IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ Microservices/IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -45,18 +45,18 @@ namespace FreeCourse.IdentityServer
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
                     AllowedScopes= { "catalog_fullpermission", "photo_stock_fullpermission",IdentityServerConstants.LocalApi.ScopeName }
                 },
-                   new Client
+                new Client
                 {
-                    ClientId = "WebMvcClientForUser",
-                    ClientName = "Asp.Net Core MVC",
-                    AllowOfflineAccess = true,
-                    ClientSecrets = { new Secret("secret".Sha256())},
-                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-                    AllowedScopes= { IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.Profile, IdentityServerConstants.StandardScopes.OfflineAccess,"roles" },
-                    AccessTokenLifetime = 1*60*60, // 1 saat 
-                    RefreshTokenExpiration = TokenExpiration.Absolute,
-                    AbsoluteRefreshTokenLifetime= (int)(DateTime.Now.AddDays(60)-DateTime.Now).TotalSeconds, // 60 gün tutsun dedik.
-                    RefreshTokenUsage = TokenUsage.ReUse
+                   ClientName="Asp.Net Core MVC",
+                    ClientId="WebMvcClientForUser",
+                    AllowOfflineAccess=true,
+                    ClientSecrets= {new Secret("secret".Sha256())},
+                    AllowedGrantTypes= GrantTypes.ResourceOwnerPassword,
+                    AllowedScopes={ "basket_fullpermission", "order_fullpermission", "gateway_fullpermission", IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId,IdentityServerConstants.StandardScopes.Profile, IdentityServerConstants.StandardScopes.OfflineAccess, IdentityServerConstants.LocalApi.ScopeName,"roles" },
+                    AccessTokenLifetime=1*60*60,
+                    RefreshTokenExpiration=TokenExpiration.Absolute,
+                    AbsoluteRefreshTokenLifetime= (int) (DateTime.Now.AddDays(60)- DateTime.Now).TotalSeconds,
+                    RefreshTokenUsage= TokenUsage.ReUse
                 }
             };
     }
