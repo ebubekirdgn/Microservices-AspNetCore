61fc3752d8b4c1ac4a9fd59927489a966b54c759
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-02-18 14:57:21 +0300

    update config.cs

--- Microservices/IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ Microservices/IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -11,26 +11,27 @@ namespace FreeCourse.IdentityServer
     public static class Config
     {
         public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
-       {
-            new ApiResource("resource_catalog"){Scopes= {"catalog_fullpermission"} },
-            new ApiResource("photo_stock_catalog"){Scopes= { "photo_stock_fullpermission" } },
-            new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
-       };
+        {
+            new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
+               new ApiResource("photo_stock_catalog"){Scopes={"photo_stock_fullpermission"}},
+               new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
+        };
 
         public static IEnumerable<IdentityResource> IdentityResources =>
                    new IdentityResource[]
                    {
-               new IdentityResources.Email(), //email
-                       new IdentityResources.OpenId(),//sub
+                       new IdentityResources.Email(),
+                       new IdentityResources.OpenId(),
                        new IdentityResources.Profile(),
-                       new IdentityResource(){Name = "roles",DisplayName = "Role", Description="Kullanıcı Rolleri",UserClaims=new[]{ "role"} },
+                       new IdentityResource(){ Name="roles", DisplayName="Roles", Description="Kullanıcı rolleri", UserClaims=new []{ "role"} }
                    };
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
             {
-                new ApiScope("catalog_fullpermission","Catalog API icin full erisim"),
-                new ApiScope("photo_stock_fullpermission","Photo Stock API icin full erisim"),
+                new ApiScope("catalog_fullpermission","Catalog API için full erişim"),
+
+                new ApiScope("photo_stock_fullpermission","Photo Stock API için full erişim"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -39,24 +40,24 @@ namespace FreeCourse.IdentityServer
             {
                 new Client
                 {
-                    ClientId = "WebMvcClient",
-                    ClientName = "Asp.Net Core MVC",
-                    ClientSecrets = { new Secret("secret".Sha256())},
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    AllowedScopes= { "catalog_fullpermission", "photo_stock_fullpermission",IdentityServerConstants.LocalApi.ScopeName }
+                   ClientName="Asp.Net Core MVC",
+                    ClientId="WebMvcClient",
+                    ClientSecrets= {new Secret("secret".Sha256())},
+                    AllowedGrantTypes= GrantTypes.ClientCredentials,
+                    AllowedScopes={ "catalog_fullpermission","photo_stock_fullpermission",IdentityServerConstants.LocalApi.ScopeName }
                 },
                    new Client
                 {
-                    ClientId = "WebMvcClientForUser",
-                    ClientName = "Asp.Net Core MVC",
-                    AllowOfflineAccess = true,
-                    ClientSecrets = { new Secret("secret".Sha256())},
-                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-                    AllowedScopes= { IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.Profile, IdentityServerConstants.StandardScopes.OfflineAccess,"roles" },
-                    AccessTokenLifetime = 1*60*60, // 1 saat
-                    RefreshTokenExpiration = TokenExpiration.Absolute,
-                    AbsoluteRefreshTokenLifetime= (int)(DateTime.Now.AddDays(60)-DateTime.Now).TotalSeconds, // 60 gün tutsun dedik.
-                    RefreshTokenUsage = TokenUsage.ReUse
+                   ClientName="Asp.Net Core MVC",
+                    ClientId="WebMvcClientForUser",
+                    AllowOfflineAccess=true,
+                    ClientSecrets= {new Secret("secret".Sha256())},
+                    AllowedGrantTypes= GrantTypes.ResourceOwnerPassword,
+                    AllowedScopes={IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId,IdentityServerConstants.StandardScopes.Profile, IdentityServerConstants.StandardScopes.OfflineAccess, IdentityServerConstants.LocalApi.ScopeName,"roles" },
+                    AccessTokenLifetime=1*60*60,
+                    RefreshTokenExpiration=TokenExpiration.Absolute,
+                    AbsoluteRefreshTokenLifetime= (int) (DateTime.Now.AddDays(60)- DateTime.Now).TotalSeconds,
+                    RefreshTokenUsage= TokenUsage.ReUse
                 }
             };
     }
