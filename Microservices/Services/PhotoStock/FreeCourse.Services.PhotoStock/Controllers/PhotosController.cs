84ee00d41dfe7b140575061a6a02634238d85626
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-02-19 00:16:42 +0300

    ports list update

--- Microservices/Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
+++ Microservices/Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
@@ -9,30 +9,37 @@ namespace FreeCourse.Services.PhotoStock.Controllers
     [ApiController]
     public class PhotosController : CustomBaseController
     {
+        [HttpPost]
         public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
         {
-            if (photo! is null && photo.Length > 0)
+            if (photo != null && photo.Length > 0)
             {
                 var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+
                 using var stream = new FileStream(path, FileMode.Create);
                 await photo.CopyToAsync(stream, cancellationToken);
-                //http://www.photostock.api.com/photos/.....jpg
+
                 var returnPath = "photos/" + photo.FileName;
 
                 PhotoDto photoDto = new() { Url = returnPath };
+
                 return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
             }
-            return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo is empty", 400));
+
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("photo is empty", 400));
         }
 
+        [HttpPost]
         public IActionResult PhotoDelete(string photoUrl)
         {
             var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
             if (!System.IO.File.Exists(path))
             {
-                return CreateActionResultInstance(Response<NoContent>.Fail("Photo not found", 404));
+                return CreateActionResultInstance(Response<NoContent>.Fail("photo not found", 404));
             }
+
             System.IO.File.Delete(path);
+
             return CreateActionResultInstance(Response<NoContent>.Success(204));
         }
     }
--- Microservices/Services/PhotoStock/FreeCourse.Services.PhotoStock/Program.cs
+++ Microservices/Services/PhotoStock/FreeCourse.Services.PhotoStock/Program.cs
@@ -16,6 +16,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseStaticFiles();
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
