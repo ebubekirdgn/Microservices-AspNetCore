572f8f7c5028a6733f6fce3a6336d06c3781844b
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-02-19 13:53:49 +0300

    RedisService class (Redis ile bağlantı kurmaktan sorumlu sınıf) close #16

--- Microservices/Services/Basket/FreeCoure.Services.Basket/Program.cs
+++ Microservices/Services/Basket/FreeCoure.Services.Basket/Program.cs
@@ -1,15 +1,24 @@
+using FreeCoure.Services.Basket.Services;
 using FreeCoure.Services.Basket.Settings;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.Configure<RedisSettings>(builder.Configuration.GetSection("RedisSettings"));
+
+builder.Services.AddSingleton<RedisService>(sp =>
+{
+    var redisSettings = sp.GetRequiredService<IOptions<RedisSettings>>().Value;
+    var redis = new RedisService(redisSettings.Host, redisSettings.Port);
+    redis.Connect();
+    return redis;
+});
 
 var app = builder.Build();
 
