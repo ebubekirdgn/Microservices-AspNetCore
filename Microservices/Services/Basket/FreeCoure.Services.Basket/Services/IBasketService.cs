d7f4875799af751a8cd6219a24a910d01f1fa744
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-02-19 13:44:18 +0300

    IBasketService interface  close#15

---
+++ Microservices/Services/Basket/FreeCoure.Services.Basket/Services/IBasketService.cs
@@ -0,0 +1,14 @@
+﻿using FreeCoure.Services.Basket.Dtos;
+using FreeCourse.Shared.Dtos;
+
+namespace FreeCoure.Services.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<Response<BasketDto>> GetBasket(string userId);
+
+        Task<Response<bool>> SaveOrUpdate(BasketDto basketDto);
+
+        Task<Response<bool>> Delete(string userId);
+    }
+}
