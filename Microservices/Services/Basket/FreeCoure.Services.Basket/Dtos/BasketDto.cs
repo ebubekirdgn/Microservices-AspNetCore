69bbb258102dafbd5013e941a0d94f8e1a1b2790
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-02-19 13:20:42 +0300

    update BasketItemDto close #20

--- Microservices/Services/Basket/FreeCoure.Services.Basket/Dtos/BasketDto.cs
+++ Microservices/Services/Basket/FreeCoure.Services.Basket/Dtos/BasketDto.cs
@@ -5,6 +5,7 @@
         public string UserId { get; set; }
         public string DiscountCode { get; set; }
         public List<BasketItemDto> basketItems { get; set; }
+
         public decimal TotalPrice
         {
             get => basketItems.Sum(x => x.Price * x.Quantity);
