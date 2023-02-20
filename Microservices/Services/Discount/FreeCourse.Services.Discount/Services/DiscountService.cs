be6ecee3c1bcd8d8f55f42ca87f6d623db9e827c
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-02-20 22:53:39 +0300

    update DiscountService

--- Microservices/Services/Discount/FreeCourse.Services.Discount/Services/DiscountService.cs
+++ Microservices/Services/Discount/FreeCourse.Services.Discount/Services/DiscountService.cs
@@ -13,20 +13,18 @@ namespace FreeCourse.Services.Discount.Services
         public DiscountService(IConfiguration configuration)
         {
             _configuration = configuration;
-
             _dbConnection = new NpgsqlConnection(_configuration.GetConnectionString("PostgreSql"));
-        }
-        public async Task<Response<NoContent>> Delete(int id)
-        {
-            var status = await _dbConnection.ExecuteAsync("delete from discount where id=@Id", new { Id = id });
-
-            return status > 0 ? Response<NoContent>.Success(204) : Response<NoContent>.Fail("Discount not found", 404);
         }
         public async Task<Response<List<Models.Discount>>> GetAll()
         {
             var discounts = await _dbConnection.QueryAsync<Models.Discount>("Select * from discount");
 
             return Response<List<Models.Discount>>.Success(discounts.ToList(), 200);
+        }
+        public async Task<Response<NoContent>> Delete(int id)
+        {
+            var status = await _dbConnection.ExecuteAsync("delete from discount where id=@Id", new { Id = id });
+            return status > 0 ? Response<NoContent>.Success(204) : Response<NoContent>.Fail("Discount not found", 404);
         }
         public async Task<Response<Models.Discount>> GetByCodeAndUserId(string code, string userId)
         {
