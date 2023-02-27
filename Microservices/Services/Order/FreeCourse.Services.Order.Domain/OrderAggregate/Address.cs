2a7acada7e03396ea75a75fe6c8ba8c6085b87b8
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-02-28 02:58:03 +0300

    update

--- Microservices/Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Address.cs
+++ Microservices/Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Address.cs
@@ -1,17 +1,14 @@
-﻿namespace FreeCourse.Services.Order.Domain.OrderAggregate
+﻿using FreeCourse.Services.Order.Domain.Core;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
 {
-    public class Address
+    public class Address:ValueObject
     {
         public string Province { get; private set; }
-
         public string District { get; private set; }
-
         public string Street { get; private set; }
-
         public string ZipCode { get; private set; }
-
         public string Line { get; private set; }
-
         public Address(string province, string district, string street, string zipCode, string line)
         {
             Province = province;
--- Microservices/Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/OrderItem.cs
+++ Microservices/Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/OrderItem.cs
@@ -2,5 +2,28 @@
 {
     public class OrderItem
     {
+        public string ProductId { get; private set; }
+        public string ProductName { get; private set; }
+        public string PictureUrl { get; private set; }
+        public Decimal Price { get; private set; }
+
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(string productId, string productName, string pictureUrl, decimal price)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            PictureUrl = pictureUrl;
+            Price = price;
+        }
+
+        public void UpdateOrderItem(string productName, string pictureUrl, decimal price)
+        {
+            ProductName = productName;
+            Price = price;
+            PictureUrl = pictureUrl;
+        }
     }
 }
