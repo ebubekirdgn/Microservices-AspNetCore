2d9fc0bd0d7a0635baaf311c707c6fc1eab9fed9
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-03-01 02:50:48 +0300

    update FreeCourse.Services.Order.Domain -> Address

--- Microservices/Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Address.cs
+++ Microservices/Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Address.cs
@@ -2,13 +2,14 @@
 
 namespace FreeCourse.Services.Order.Domain.OrderAggregate
 {
-    public class Address:ValueObject
+    public class Address : ValueObject
     {
         public string Province { get; private set; }
         public string District { get; private set; }
         public string Street { get; private set; }
         public string ZipCode { get; private set; }
         public string Line { get; private set; }
+
         public Address(string province, string district, string street, string zipCode, string line)
         {
             Province = province;
