a5dd7a5caa1439766972d7f555b1de1be5a14cb5
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-03-01 02:44:29 +0300

    cleaned

--- Microservices/Services/Order/FreeCourse.Services.Order.Domain.Core/ValueObject.cs
+++ Microservices/Services/Order/FreeCourse.Services.Order.Domain.Core/ValueObject.cs
@@ -10,14 +10,11 @@
             }
             return ReferenceEquals(left, null) || left.Equals(right);
         }
-
         protected static bool NotEqualOperator(ValueObject left, ValueObject right)
         {
             return !(EqualOperator(left, right));
         }
-
         protected abstract IEnumerable<object> GetEqualityComponents();
-
         public override bool Equals(object obj)
         {
             if (obj == null || obj.GetType() != GetType())
@@ -29,14 +26,12 @@
 
             return this.GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
         }
-
         public override int GetHashCode()
         {
             return GetEqualityComponents()
              .Select(x => x != null ? x.GetHashCode() : 0)
              .Aggregate((x, y) => x ^ y);
         }
-
         public ValueObject GetCopy()
         {
             return this.MemberwiseClone() as ValueObject;
