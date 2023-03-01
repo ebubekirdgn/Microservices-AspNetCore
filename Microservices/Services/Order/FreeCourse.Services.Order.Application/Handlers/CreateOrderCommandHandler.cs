775f603a0073769cc0b56ac932082e5bf9d22f8f
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-03-02 00:39:37 +0300

    create CreateOrderCommandHandler #43

---
+++ Microservices/Services/Order/FreeCourse.Services.Order.Application/Handlers/CreateOrderCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using FreeCourse.Services.Order.Application.Dtos;
+using FreeCourse.Services.Order.Infrastructure;
+using FreeCourse.Shared.Dtos;
+using MediatR;
+
+namespace FreeCourse.Services.Order.Application.Handlers
+{
+    public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, Response<CreatedOrderDto>>
+    {
+        private readonly OrderDbContext _context;
+
+        public CreateOrderCommandHandler(OrderDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Response<CreatedOrderDto>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
