1ee591e4d3da0e0dabe4a546843d0d995ff352d7
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-03-02 00:30:24 +0300

    update

--- Microservices/Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Order.cs
+++ Microservices/Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Order.cs
@@ -9,19 +9,14 @@ namespace FreeCourse.Services.Order.Domain.OrderAggregate
     public class Order : Entity, IAggregateRoot
     {
         public DateTime CreatedDate { get; private set; }
-
         public Address Address { get; private set; }
-
         public string BuyerId { get; private set; }
 
         private readonly List<OrderItem> _orderItems;
-
         public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
-
         public Order()
         {
         }
-
         public Order(string buyerId, Address address)
         {
             _orderItems = new List<OrderItem>();
@@ -29,7 +24,6 @@ namespace FreeCourse.Services.Order.Domain.OrderAggregate
             BuyerId = buyerId;
             Address = address;
         }
-
         public void AddOrderItem(string productId, string productName, decimal price, string pictureUrl)
         {
             var existProduct = _orderItems.Any(x => x.ProductId == productId);
@@ -41,7 +35,6 @@ namespace FreeCourse.Services.Order.Domain.OrderAggregate
                 _orderItems.Add(newOrderItem);
             }
         }
-
         public decimal GetTotalPrice => _orderItems.Sum(x => x.Price);
     }
 }
