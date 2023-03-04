68909c890f2ee0b4f330c311a46db855bb2bd182
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-03-04 10:52:51 +0300

    create configuration.development.json

--- Microservices/FreeCourse.Gateway/Program.cs
+++ Microservices/FreeCourse.Gateway/Program.cs
@@ -1,6 +1,10 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+builder.Services.AddOcelot();
 
 app.MapGet("/", () => "Hello World!");
-
+await app.UseOcelot();
 app.Run();
