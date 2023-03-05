2d122ab19cdc7588ab2df21e917c905e027ce94d
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-03-05 23:18:37 +0300

    sign in page create

---
+++ Microservices/Frontends/FreeCourse.Web/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using FreeCourse.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
