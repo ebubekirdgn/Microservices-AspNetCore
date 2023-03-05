3995f44d5d54453792e68448fc10ac6f3abc614c
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-03-05 14:43:45 +0300

    create SigninInput

---
+++ Microservices/Frontends/FreeCourse.Web/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,13 @@
+﻿using FreeCourse.Shared.Dtos;
+using FreeCourse.Web.Models;
+using IdentityModel.Client;
+
+namespace FreeCourse.Web.Services.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<Response<bool>> SignIn(SignInInput signinInput);
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+        Task RevokeRefreshToken(); // Loguout oldugumuz zaman refresh tokeni sileceğiz
+    }
+}
