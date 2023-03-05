cf73791174cfc2d9341c348f2889a87680285dc9
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-03-05 23:52:00 +0300

    add Logout method  int AuthController

--- Microservices/Frontends/FreeCourse.Web/Controllers/AuthController.cs
+++ Microservices/Frontends/FreeCourse.Web/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 ﻿using FreeCourse.Web.Models;
 using FreeCourse.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FreeCourse.Web.Controllers
@@ -35,6 +37,13 @@ namespace FreeCourse.Web.Controllers
                 return View();
             }
             return RedirectToAction(nameof(Index), "Home");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await _identityService.RevokeRefreshToken();
+            return RedirectToAction(nameof(HomeController.Index), "Home");
         }
     }
 }
