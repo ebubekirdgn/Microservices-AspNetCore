88e33c3cdce778b1fc14012db7579fac465a6fb5
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-03-06 22:45:49 +0300

    cleaned code

--- Microservices/IdentityServer/FreeCourse.IdentityServer/SeedData.cs
+++ Microservices/IdentityServer/FreeCourse.IdentityServer/SeedData.cs
@@ -1,17 +1,16 @@
 ﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-
-using System;
-using System.Linq;
-using System.Security.Claims;
-using IdentityModel;
 using FreeCourse.IdentityServer.Data;
 using FreeCourse.IdentityServer.Models;
+using IdentityModel;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
+using System;
+using System.Linq;
+using System.Security.Claims;
 
 namespace FreeCourse.IdentityServer
 {
--- Microservices/IdentityServer/FreeCourse.IdentityServer/Startup.cs
+++ Microservices/IdentityServer/FreeCourse.IdentityServer/Startup.cs
@@ -1,9 +1,10 @@
 ﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-using IdentityServer4;
 using FreeCourse.IdentityServer.Data;
 using FreeCourse.IdentityServer.Models;
+using FreeCourse.IdentityServer.Services;
+using IdentityServer4;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -11,7 +12,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using FreeCourse.IdentityServer.Services;
 
 namespace FreeCourse.IdentityServer
 {
