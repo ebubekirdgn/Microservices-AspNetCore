ffcf46e122e6b0098a7c2016f55fd38296ecfa62
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-03-08 20:44:26 +0300

    create CatalogService

---
+++ Microservices/Frontends/FreeCourse.Web/Services/Catalog/CatalogService.cs
@@ -0,0 +1,38 @@
+﻿using FreeCourse.Web.Models.Catalogs;
+using FreeCourse.Web.Services.Interfaces;
+
+namespace FreeCourse.Web.Services.Catalog
+{
+    public class CatalogService : ICatalogService
+    {
+        public Task<bool> CreateCourseAsync(CourseCreateInput courseCreateInput)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCourseAsync(string courseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CourseViewModel>> GetAllCourseAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CourseViewModel>> GetAllCourseByUserIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CourseViewModel> GetByCourseId(string courseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateCourseAsync(CourseUpdateInput courseUpdateInput)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
