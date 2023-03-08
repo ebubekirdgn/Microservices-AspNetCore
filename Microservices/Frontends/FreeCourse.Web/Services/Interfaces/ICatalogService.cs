879b567170a662dfcc74dbd26e38344d10b540ea
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-03-08 20:41:17 +0300

    fill ICatalogService

--- Microservices/Frontends/FreeCourse.Web/Services/Interfaces/ICatalogService.cs
+++ Microservices/Frontends/FreeCourse.Web/Services/Interfaces/ICatalogService.cs
@@ -5,11 +5,11 @@ namespace FreeCourse.Web.Services.Interfaces
     public interface ICatalogService
     {
         Task<List<CourseViewModel>> GetAllCourseAsync();
+        //Task<List<CategoryViewModel>> GetAllCategoryAsync();
         Task<List<CourseViewModel>> GetAllCourseByUserIdAsync(string userId);
-        
         Task<CourseViewModel> GetByCourseId(string courseId);
+        Task<bool> CreateCourseAsync(CourseCreateInput courseCreateInput);
+        Task<bool> UpdateCourseAsync(CourseUpdateInput courseUpdateInput);
         Task<bool> DeleteCourseAsync(string courseId);
-
-
     }
 }
