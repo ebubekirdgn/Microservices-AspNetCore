84df5c9f73cebe62c249a44e8c4b8be56316a02c
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-03-09 20:17:28 +0300

    create ClientCredentialTokenService

---
+++ Microservices/Frontends/FreeCourse.Web/Services/ClientCredentialTokenService.cs
@@ -0,0 +1,12 @@
+﻿using FreeCourse.Web.Services.Interfaces;
+
+namespace FreeCourse.Web.Services
+{
+    public class ClientCredentialTokenService : IClientCredentialTokenService
+    {
+        public Task<string> GetToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
