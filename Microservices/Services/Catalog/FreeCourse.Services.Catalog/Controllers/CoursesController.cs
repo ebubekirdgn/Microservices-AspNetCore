59b537993a01ce93d7e0cfe2e40dd85430467795
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-03-28 00:24:11 +0300

    update

--- Microservices/Services/Catalog/FreeCourse.Services.Catalog/Controllers/CoursesController.cs
+++ Microservices/Services/Catalog/FreeCourse.Services.Catalog/Controllers/CoursesController.cs
@@ -28,7 +28,6 @@ namespace FreeCourse.Services.Catalog.Controllers
         public async Task<IActionResult> GetById(string id)
         {
             var response = await _courseService.GetByIdAsync(id);
-
             return CreateActionResultInstance(response);
         }
 
@@ -37,7 +36,6 @@ namespace FreeCourse.Services.Catalog.Controllers
         public async Task<IActionResult> GetAllByUserId(string userId)
         {
             var response = await _courseService.GetAllByUserIdAsync(userId);
-
             return CreateActionResultInstance(response);
         }
 
@@ -52,7 +50,6 @@ namespace FreeCourse.Services.Catalog.Controllers
         public async Task<IActionResult> Update(CourseUpdateDto courseUpdateDto)
         {
             var response = await _courseService.UpdateAsync(courseUpdateDto);
-
             return CreateActionResultInstance(response);
         }
 
@@ -60,7 +57,6 @@ namespace FreeCourse.Services.Catalog.Controllers
         public async Task<IActionResult> Delete(string id)
         {
             var response = await _courseService.DeleteAsync(id);
-
             return CreateActionResultInstance(response);
         }
     }
