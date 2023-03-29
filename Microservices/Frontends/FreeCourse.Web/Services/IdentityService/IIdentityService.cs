b0f8fe57d3904ebc6a04d6b51ce89281cab735f6
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-03-29 20:04:50 +0300

    create IIdentityService

---
+++ Microservices/Frontends/FreeCourse.Web/Services/IdentityService/IIdentityService.cs
@@ -0,0 +1,12 @@
+﻿using FreeCourse.Shared.Dtos;
+using IdentityModel.Client;
+
+namespace FreeCourse.Web.Services.IdentityService
+{
+    public interface IIdentityService
+    {
+        Task<Response<bool>> SignIn();
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+        Task RevokeRefreshToken();
+    }
+}
