047780791e8dd31d6e8c7f2a2a37f78567bee78b
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-03-30 22:47:57 +0300

    create IdentityServer

--- Microservices/IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ Microservices/IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -9,53 +9,18 @@ namespace FreeCourse.IdentityServer
 {
     public static class Config
     {
-<<<<<<< Updated upstream
-        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
-        {
-            new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
-               new ApiResource("resource_photo_stock"){Scopes={"photo_stock_fullpermission"}},
-                  new ApiResource("resource_basket"){Scopes={"basket_fullpermission"}},
-                      new ApiResource("resource_discount"){Scopes={"discount_fullpermission"}},
-                        new ApiResource("resource_order"){Scopes={"order_fullpermission"}},
-                             new ApiResource("resource_payment"){Scopes={"payment_fullpermission"}},
-                                new ApiResource("resource_gateway"){Scopes={"gateway_fullpermission"}},
-                                 new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
-        };
-
-        public static IEnumerable<IdentityResource> IdentityResources =>
-                   new IdentityResource[]
-                   {
-                       new IdentityResources.Email(),
-                       new IdentityResources.OpenId(),
-                       new IdentityResources.Profile(),
-                       new IdentityResource(){ Name="roles", DisplayName="Roles", Description="Kullanıcı rolleri", UserClaims=new []{ "role"} }
-=======
         public static IEnumerable<IdentityResource> IdentityResources =>
                    new IdentityResource[]
                    {
                 new IdentityResources.OpenId(),
                 new IdentityResources.Profile(),
->>>>>>> Stashed changes
                    };
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
             {
-<<<<<<< Updated upstream
-                new ApiScope("catalog_fullpermission","Catalog API için full erişim"),
-
-                new ApiScope("photo_stock_fullpermission","Photo Stock API için full erişim"),
-
-                     new ApiScope("basket_fullpermission","Basket API için full erişim"),
-                            new ApiScope("discount_fullpermission","Discount API için full erişim"),
-                             new ApiScope("order_fullpermission","Order API için full erişim"),
-                                 new ApiScope("payment_fullpermission","Payment API için full erişim"),
-                                   new ApiScope("gateway_fullpermission","Gateway API için full erişim"),
-                new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
-=======
                 new ApiScope("scope1"),
                 new ApiScope("scope2"),
->>>>>>> Stashed changes
             };
 
         public static IEnumerable<Client> Clients =>
@@ -72,20 +37,6 @@ namespace FreeCourse.IdentityServer
 
                     AllowedScopes = { "scope1" }
                 },
-<<<<<<< Updated upstream
-                   new Client
-                {
-                   ClientName="Asp.Net Core MVC",
-                    ClientId="WebMvcClientForUser",
-                    AllowOfflineAccess=true,
-                    ClientSecrets= {new Secret("secret".Sha256())},
-                    AllowedGrantTypes= GrantTypes.ResourceOwnerPassword,
-                    AllowedScopes={ "basket_fullpermission", "order_fullpermission", "gateway_fullpermission", IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId,IdentityServerConstants.StandardScopes.Profile, IdentityServerConstants.StandardScopes.OfflineAccess, IdentityServerConstants.LocalApi.ScopeName,"roles" },
-                    AccessTokenLifetime=1*60*60,
-                    RefreshTokenExpiration=TokenExpiration.Absolute,
-                    AbsoluteRefreshTokenLifetime= (int) (DateTime.Now.AddDays(60)- DateTime.Now).TotalSeconds,
-                    RefreshTokenUsage= TokenUsage.ReUse
-=======
 
                 // interactive client using code flow + pkce
                 new Client
@@ -101,15 +52,6 @@ namespace FreeCourse.IdentityServer
 
                     AllowOfflineAccess = true,
                     AllowedScopes = { "openid", "profile", "scope2" }
->>>>>>> Stashed changes
-                },
-                      new Client
-                {
-                   ClientName="Token Exchange Client",
-                    ClientId="TokenExhangeClient",
-                    ClientSecrets= {new Secret("secret".Sha256())},
-                    AllowedGrantTypes= new []{ "urn:ietf:params:oauth:grant-type:token-exchange" },
-                    AllowedScopes={ "discount_fullpermission", "payment_fullpermission", IdentityServerConstants.StandardScopes.OpenId }
                 },
             };
     }
--- Microservices/IdentityServer/FreeCourse.IdentityServer/Program.cs
+++ Microservices/IdentityServer/FreeCourse.IdentityServer/Program.cs
@@ -3,11 +3,6 @@
 
 
 using Microsoft.AspNetCore.Hosting;
-<<<<<<< Updated upstream
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-=======
->>>>>>> Stashed changes
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
