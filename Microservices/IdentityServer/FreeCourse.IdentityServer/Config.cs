f7ea14d4bf3e7f9e1d7efd5396ed642581eba21e
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-03-30 21:29:50 +0300

    update

--- Microservices/IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ Microservices/IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -1,15 +1,15 @@
 ﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-using IdentityServer4;
+
 using IdentityServer4.Models;
-using System;
 using System.Collections.Generic;
 
 namespace FreeCourse.IdentityServer
 {
     public static class Config
     {
+<<<<<<< Updated upstream
         public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
         {
             new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
@@ -29,11 +29,19 @@ namespace FreeCourse.IdentityServer
                        new IdentityResources.OpenId(),
                        new IdentityResources.Profile(),
                        new IdentityResource(){ Name="roles", DisplayName="Roles", Description="Kullanıcı rolleri", UserClaims=new []{ "role"} }
+=======
+        public static IEnumerable<IdentityResource> IdentityResources =>
+                   new IdentityResource[]
+                   {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+>>>>>>> Stashed changes
                    };
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
             {
+<<<<<<< Updated upstream
                 new ApiScope("catalog_fullpermission","Catalog API için full erişim"),
 
                 new ApiScope("photo_stock_fullpermission","Photo Stock API için full erişim"),
@@ -44,19 +52,27 @@ namespace FreeCourse.IdentityServer
                                  new ApiScope("payment_fullpermission","Payment API için full erişim"),
                                    new ApiScope("gateway_fullpermission","Gateway API için full erişim"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
+=======
+                new ApiScope("scope1"),
+                new ApiScope("scope2"),
+>>>>>>> Stashed changes
             };
 
         public static IEnumerable<Client> Clients =>
             new Client[]
             {
+                // m2m client credentials flow client
                 new Client
                 {
-                   ClientName="Asp.Net Core MVC",
-                    ClientId="WebMvcClient",
-                    ClientSecrets= {new Secret("secret".Sha256())},
-                    AllowedGrantTypes= GrantTypes.ClientCredentials,
-                    AllowedScopes={ "catalog_fullpermission","photo_stock_fullpermission", "gateway_fullpermission", IdentityServerConstants.LocalApi.ScopeName }
+                    ClientId = "m2m.client",
+                    ClientName = "Client Credentials Client",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+                    AllowedScopes = { "scope1" }
                 },
+<<<<<<< Updated upstream
                    new Client
                 {
                    ClientName="Asp.Net Core MVC",
@@ -69,6 +85,23 @@ namespace FreeCourse.IdentityServer
                     RefreshTokenExpiration=TokenExpiration.Absolute,
                     AbsoluteRefreshTokenLifetime= (int) (DateTime.Now.AddDays(60)- DateTime.Now).TotalSeconds,
                     RefreshTokenUsage= TokenUsage.ReUse
+=======
+
+                // interactive client using code flow + pkce
+                new Client
+                {
+                    ClientId = "interactive",
+                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    RedirectUris = { "https://localhost:44300/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+
+                    AllowOfflineAccess = true,
+                    AllowedScopes = { "openid", "profile", "scope2" }
+>>>>>>> Stashed changes
                 },
                       new Client
                 {
