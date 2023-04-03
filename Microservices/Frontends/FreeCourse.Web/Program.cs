314447672b2221e10f7de0d1159ac4e6817acf4e
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-04-03 23:10:57 +0300

    update layout

--- Microservices/Frontends/FreeCourse.Web/Program.cs
+++ Microservices/Frontends/FreeCourse.Web/Program.cs
@@ -15,7 +15,9 @@ builder.Services.AddHttpContextAccessor();
 
 var serviceApiSettings = builder.Configuration.GetSection("ServiceApiSettings").Get<ServiceApiSettings>();
 
+builder.Services.AddScoped<ResourceOwnerPasswordTokenHandler>();
 builder.Services.AddHttpClient<IIdentityService, IdentityService>();
+
 builder.Services.AddHttpClient<IUserService, UserService>(opt =>
 {
     opt.BaseAddress = new Uri(serviceApiSettings.IdentityBaseUri);
