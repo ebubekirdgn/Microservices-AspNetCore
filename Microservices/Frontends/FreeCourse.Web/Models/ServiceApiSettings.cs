9c435dc23006ef2005311789196f0c1aca6ef4e9
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-04-03 23:55:53 +0300

    microservice request add

--- Microservices/Frontends/FreeCourse.Web/Models/ServiceApiSettings.cs
+++ Microservices/Frontends/FreeCourse.Web/Models/ServiceApiSettings.cs
@@ -7,13 +7,9 @@
         public string PhotoStockUri { get; set; }
 
         public ServiceApi Catalog { get; set; }
-
         public ServiceApi PhotoStock { get; set; }
-
         public ServiceApi Basket { get; set; }
-
         public ServiceApi Discount { get; set; }
-
         public ServiceApi Payment { get; set; }
         public ServiceApi Order { get; set; }
     }
---
+++ Microservices/Frontends/FreeCourse.Web/Services/CatalogService.cs
@@ -0,0 +1,50 @@
+﻿using FreeCourse.Web.Models.Catalogs;
+using FreeCourse.Web.Services.Interfaces;
+
+namespace FreeCourse.Web.Services
+{
+    public class CatalogService : ICatalogService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CatalogService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task<bool> CreateCourseAsync(CourseCreateInput courseCreateInput)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCourseAsync(string courseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CategoryViewModel>> GetAllCategoryAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CourseViewModel>> GetAllCourseAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CourseViewModel>> GetAllCourseByUserIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CourseViewModel> GetByCourseId(string courseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateCourseAsync(CourseUpdateInput courseUpdateInput)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
