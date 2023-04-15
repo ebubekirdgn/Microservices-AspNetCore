4ff500c304cc6ef171f9bb5f87c70bf44f9bbff8
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-04-15 10:58:13 +0300

    create Validator

--- Microservices/Frontends/FreeCourse.Web/Program.cs
+++ Microservices/Frontends/FreeCourse.Web/Program.cs
@@ -1,8 +1,10 @@
+using FluentValidation.AspNetCore;
 using FreeCourse.Shared.Services;
 using FreeCourse.Web.Extensions;
 using FreeCourse.Web.Handler;
 using FreeCourse.Web.Helpers;
 using FreeCourse.Web.Models;
+using FreeCourse.Web.Validators;
 using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -31,6 +33,8 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
     opts.Cookie.Name = "udemywebcookie";
 });
 
+
+builder.Services.AddControllersWithViews().AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<CourseCreateInputValidator>());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
