b83a02f2e5a25e76261a4b7d6d91d7d45fa095a8
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-04-15 11:00:35 +0300

    update

--- Microservices/Frontends/FreeCourse.Web/Models/Catalogs/CourseCreateInput.cs
+++ Microservices/Frontends/FreeCourse.Web/Models/Catalogs/CourseCreateInput.cs
@@ -7,21 +7,17 @@ namespace FreeCourse.Web.Models.Catalogs
         public string? UserId { get; set; }
 
         [Display(Name = "Kurs Kategori")]
-        [Required]
         public string CategoryId { get; set; }
 
         [Display(Name = "Kurs İsmi")]
-        [Required]
         public string Name { get; set; }
 
         [Display(Name = "Kurs Açıklama")]
-        [Required]
         public string Description { get; set; }
 
         public string? Picture { get; set; }
 
         [Display(Name = "Kurs Ücreti (₺) ")]
-        [Required]
         public decimal Price { get; set; }
 
         public FeatureViewModel Feature { get; set; }
