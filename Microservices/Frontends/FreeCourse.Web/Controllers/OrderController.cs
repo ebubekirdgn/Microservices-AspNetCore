16797a118265b657446729d3ff3eb8be571193c7
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-04-20 05:03:00 +0300

    Basket cshtml update

--- Microservices/Frontends/FreeCourse.Web/Controllers/OrderController.cs
+++ Microservices/Frontends/FreeCourse.Web/Controllers/OrderController.cs
@@ -15,12 +15,44 @@ namespace FreeCourse.Web.Controllers
             _basketService = basketService;
             _orderService = orderService;
         }
+
         public async Task<IActionResult> Checkout()
         {
             var basket = await _basketService.Get();
 
             ViewBag.basket = basket;
             return View(new CheckoutInfoInput());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Checkout(CheckoutInfoInput checkoutInfoInput)
+        {
+            //1. yol senkron iletişim
+            //  var orderStatus = await _orderService.CreateOrder(checkoutInfoInput);
+            // 2.yol asenkron iletişim
+            var orderSuspend = await _orderService.SuspendOrder(checkoutInfoInput);
+            if (!orderSuspend.IsSuccessful)
+            {
+                var basket = await _basketService.Get();
+                ViewBag.basket = basket;
+                ViewBag.error = orderSuspend.Error;
+                return View();
+            }
+            //1. yol senkron iletişim
+            //  return RedirectToAction(nameof(SuccessfulCheckout), new { orderId = orderStatus.OrderId });
+
+            //2.yol asenkron iletişim
+            return RedirectToAction(nameof(SuccessfulCheckout), new { orderId = new Random().Next(1, 1000) });
+        }
+        public IActionResult SuccessfulCheckout(int orderId)
+        {
+            ViewBag.orderId = orderId;
+            return View();
+        }
+        public async Task<IActionResult> CheckoutHistory()
+        {
+            var order = await _orderService.GetOrder()
+            return View(order);
         }
     }
 }
